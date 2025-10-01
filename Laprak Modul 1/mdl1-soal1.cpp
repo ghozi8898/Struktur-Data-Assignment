a80fec796959de2d6b9f8bb6fadf8a22fba48194
Ghozi <fathirghozy@gmail.com>
2025-10-01 18:40:55 +0700

    Laprak Modul 1/mdl1-soal1.cpp

---
+++ Laprak Modul 1/mdl1-soal1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a;
+    float b;
+
+    cout << "A: ";
+    cin >> a;
+    cout << "B: ";
+    cin >> b;
+
+    // Operasi aritmetika
+    cout << (a + b) << endl;
+    cout << (a - b) << endl;
+    cout << (a * b) << endl;
+    cout << (a / b) << endl;
+    
+    return 0;
+}
