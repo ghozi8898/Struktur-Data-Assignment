01c3419931fa70c18481c2012f9fe95403142265
Ghozi <fathirghozy@gmail.com>
2025-10-01 18:22:09 +0700

    Fix_module1

---
+++ Modul1/acbec.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct saham{
+    string nama_emiten;
+    saham *next;
+}*first,*last,*current;
+
+int main() {
+    current = new saham;
+    current->nama_emiten = "AXI0";
+    first=current;
+    last=current;
+
+    current = new saham;
+    current->nama_emiten = "TLKM";
+    last->next=current;
+    last=current;
+
+    current = new saham;
+    current ->nama_emiten = "PIBA";
+    last ->next=current;
+    last=current;
+
+    current = new saham;
+    current ->nama_emiten = "MDF";
+    last ->next=current;
+    last=current;
+
+    current ->next=NULL;
+    last->next=NULL;
+
+    current = first;
+
+    while(current!=NULL){
+        cout << current->nama_emiten << endl;
+        current=current->next;
+    }
+}
